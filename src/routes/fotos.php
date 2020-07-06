b0ee15998db9b82a8597aa463f63d886a350f681
federicodonner <federico.donner@gmail.com>
2020-07-05 21:06:01 -0300

    Funcionalidad terminada
    - Descarga adjuntos, sin duplicados
    - Devuelve listado de 10 últimos adjuntos
    - Borra emails antíguos

---
+++ src/routes/fotos.php
@@ -0,0 +1,142 @@
+<?php
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+// Todas los fotos
+$app->get('/api/fotos/{identificador}', function (Request $request, Response $response) {
+    $identificador = $request->getAttribute('identificador');
+    /* connect to mail */
+    $hostname = '{mail.federicodonner.com:993/imap/ssl/novalidate-cert}INBOX';
+    $username = $identificador.'@federicodonner.com';
+    $password = '010203040506070809';
+
+
+
+    /* try to connect */
+    $inbox = imap_open($hostname, $username, $password);
+
+    if ($inbox) {
+        /* grab emails */
+        $emails = imap_search($inbox, 'ALL');
+
+        /* if emails are returned, cycle through each... */
+        if ($emails) {
+
+        /* begin output var */
+            $output = '';
+
+            /* put the newest emails on top */
+            rsort($emails);
+            $cuenta = 0;
+            $nombresArchivos = array();
+            /* for every email... */
+            foreach ($emails as $email_number) {
+
+            /* get information specific to this email */
+                $overview = imap_fetch_overview($inbox, $email_number, 0);
+
+                $message = imap_fetchbody($inbox, $email_number, 2);
+
+                /* get mail structure */
+                $structure = imap_fetchstructure($inbox, $email_number);
+
+                $attachments = array();
+
+                /* if any attachments found... */
+                if (isset($structure->parts) && count($structure->parts)) {
+                    for ($i = 0; $i < count($structure->parts); $i++) {
+                        $attachments[$i] = array(
+                     'is_attachment' => false,
+                     'filename' => '',
+                     'name' => '',
+                     'attachment' => ''
+                 );
+
+                        if ($structure->parts[$i]->ifdparameters) {
+                            foreach ($structure->parts[$i]->dparameters as $object) {
+                                if (strtolower($object->attribute) == 'filename') {
+                                    $attachments[$i]['is_attachment'] = true;
+                                    $attachments[$i]['filename'] = $object->value;
+                                }
+                            }
+                        }
+
+                        if ($structure->parts[$i]->ifparameters) {
+                            foreach ($structure->parts[$i]->parameters as $object) {
+                                if (strtolower($object->attribute) == 'name') {
+                                    $attachments[$i]['is_attachment'] = true;
+                                    // $attachments[$i]['name'] = $object->value;
+                                    $attachments[$i]['name'] = $object->value;
+                                }
+                            }
+                        }
+
+                        if ($attachments[$i]['is_attachment']) {
+                            $attachments[$i]['attachment'] = imap_fetchbody($inbox, $email_number, $i+1);
+
+                            /* 3 = BASE64 encoding */
+                            if ($structure->parts[$i]->encoding == 3) {
+                                $attachments[$i]['attachment'] = base64_decode($attachments[$i]['attachment']);
+                            }
+                            /* 4 = QUOTED-PRINTABLE encoding */
+                            elseif ($structure->parts[$i]->encoding == 4) {
+                                $attachments[$i]['attachment'] = quoted_printable_decode($attachments[$i]['attachment']);
+                            }
+                        }
+                    }
+                }
+
+
+                /* iterate through each attachment and save it */
+                foreach ($attachments as $attachment) {
+                    $borrarEmail = false;
+                    if ($cuenta < 10) {
+                        if ($attachment['is_attachment'] == 1) {
+                            $filename = $attachment['name'];
+                            if (empty($filename)) {
+                                $filename = $attachment['filename'];
+                            }
+
+                            if (empty($filename)) {
+                                $filename = time() . ".dat";
+                            }
+                            $folder = $identificador;
+                            if (!is_dir($folder)) {
+                                mkdir($folder);
+                            }
+                            // Verifica que el archivo exista por si hay dos adjuntos con el mismo nombre
+                            if (!file_exists("./". $folder ."/" . $filename)) {
+                                $fp = fopen("./". $folder ."/" . $filename, "w+");
+                                fwrite($fp, $attachment['attachment']);
+                                fclose($fp);
+
+                                // Si guarda el archivo, guarda el nombre para responder el request
+                                array_push($nombresArchivos, $filename);
+
+                                $cuenta=$cuenta+1;
+                            }
+                        }
+                    } else {
+                        // Si es un mail mayor al décimo, lo marca para borrar
+                        $borrarEmail = true;
+                    }
+                }
+                if ($borrarEmail) {
+                    // Borra el mail marcado
+                    $structure = imap_delete($inbox, $email_number);
+                }
+            }
+        }
+    } else {
+        return respondWithError($response, 'Hubo un error recuperando los archivos. Avisale a Fefi.', 400);
+    }
+
+    // Expunge borra los mails marcados para borrar
+    imap_expunge($inbox);
+
+    /* close the connection */
+    imap_close($inbox);
+    $objetoRespuesta->nombres = $nombresArchivos;
+    return dataResponse($response, $objetoRespuesta);
+});
